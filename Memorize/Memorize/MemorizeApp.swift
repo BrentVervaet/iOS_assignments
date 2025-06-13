8f869b3b5ca9fbb2cd517d06f417f0e9cc007690
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-06-13 16:13:50 +0200

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Brent Vervaet on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
