1b6e6e8861ffd6bed58e3fe4d7c3349014554164
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-06-17 19:23:16 +0200

    model

--- Memorize/Memorize/MemorizeGame.swift
+++ Memorize/Memorize/MemorizeGame.swift
@@ -7,4 +7,21 @@
 
 import Foundation
 
+struct MemorizeGame<CardContent> {
+	var cards : Array<Card>
+	
+	func choose(_ card: Card){
+		
+	}
+	
+	func score() -> Int {
+		return 0
+	}
+	
+	struct Card {
+		var isFaceUp: Bool
+		var isMatched: Bool
+		var content: CardContent
+	}
+}
 
